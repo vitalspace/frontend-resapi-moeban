d59c25ad0fe92ad6ea46fbfd21276bc4cf1d3f7b
vitalspace <myglobalsounds@gmail.com>
2023-07-12 14:52:33 -0600

    Added css transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
+  import { isOpen } from "./stores/stores";
 
-import User from "./components/user.svelte";
+  import AddUser from "./components/addUser.svelte";
+  import User from "./components/user.svelte";
+
+  const openModal = () => ($isOpen = !$isOpen);
 </script>
 
-
-<div class="flex min-h-screen items-center justify-center bg-gradient-to-br from-rose-100 to-teal-100">
-  <div class="w-1/2 rounded-lg shadow-2xl bg-zinc-100 p-2 text-center">
+<div
+  class="flex min-h-screen items-center justify-center bg-gradient-to-br from-rose-100 to-teal-100"
+>
+  <div class="sm:w-1/2 sm:mx-0 w-full mx-4 rounded-lg shadow-2xl bg-zinc-100 p-2 text-center">
     <section class="py-4">
-      <h1 class="text-3xl text-gray-600 font-bold ">Bun ResApi + Moeban DB.</h1>
+      <h1 class="text-3xl text-gray-600 font-bold">Bun ResApi + Moeban DB.</h1>
     </section>
 
     <section>
       <div class="text-center py-4">
-        <button class="flex justify-center items-center mx-auto space-x-2"><img class="w-8 h-8" src="add-circle-svgrepo-com.svg" alt=""><p>Add User</p></button>
+        <button
+          on:click={() => openModal()}
+          class="flex justify-center items-center mx-auto space-x-2 bg-cyan-400 text-white font-bold rounded-full"
+          ><img class="w-8 h-8 hover:rotate-45 transition-all" src="add-circle-svgrepo-com.svg" alt="" />
+          <p class="pr-2">Add User</p></button
+        >
       </div>
-
       <div class="space-y-2">
-
-        <User id={ 1 } img={"aksdjdljka"} name={"Lucas"} occupation={"Student"} bio={"i like the video games"} />
-
+        <User
+          id={1}
+          img={"aksdjdljka"}
+          name={"Lucas"}
+          occupation={"Student"}
+          bio={"i like the video games"}
+        />
       </div>
     </section>
   </div>
 </div>
+
+{#if $isOpen}
+  <AddUser />
+{/if}
---
+++ src/components/addUser.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { isOpen } from "../stores/stores";
+
+  const closeModal = () => {
+    $isOpen = false;
+  };
+</script>
+
+<div
+  class="absolute inset-0 flex items-center justify-center bg-slate-700 bg-opacity-20"
+>
+  <div class="sm:w-2/5 w-full sm:mx-0 mx-4 rounded-lg border-2 bg-white py-2">
+    <div class="pr-2 text-right">
+      <button
+        on:click={() => closeModal()}
+
+        ><img class="w-6" src="cancel-circle-svgrepo-com.svg" alt="" /></button
+      >
+    </div>
+    <h2 class="text-ce py-4 text-center text-2xl font-bold">Add User</h2>
+    <div class="flex flex-col space-y-2 px-2">
+      <input
+        class="border-2 p-2"
+        type="text"
+        name=""
+        id=""
+        placeholder="Name"
+      />
+      <input class="border-2 p-2" type="" name="" id="" placeholder="img" />
+      <input
+        class="border-2 p-2"
+        type="text"
+        name=""
+        id=""
+        placeholder="email"
+      />
+      <textarea class="border-2 p-2" name="" id="" rows="5" />
+      <button
+        class="border-2 bg-green-300 py-2 font-bold text-white transition-all hover:bg-green-500"
+        >Submit</button
+      >
+    </div>
+  </div>
+</div>
