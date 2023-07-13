8211299d2754346a0023f0cff8f8872ce43aad08
vitalspace <myglobalsounds@gmail.com>
2023-07-12 23:47:30 -0600

    addeds deleteUser and updateUser foo

--- src/components/user.svelte
+++ src/components/user.svelte
@@ -3,6 +3,14 @@
   export let img: string;
   export let name: string;
   export let bio: string;
+
+  const deleteUser = async (id) => {
+    alert(id);
+  };
+
+  const updateUser = async (id) => {
+    alert(id);
+  };
 </script>
 
 <div class="flex items-center space-x-2 border-2 rounded-md p-2 font-serif">
@@ -10,9 +18,14 @@
   <div class="w-full text-left">
     <div class="flex justify-between">
       <p>id: {id}</p>
-      <div class="mx-2 flex space-x-4 ">
-        <button><img class="w-6" src="update-alt-2-svgrepo-com.svg" alt=""></button>
-        <button><img class="w-6 h-6" src="delete-2-svgrepo-com.svg" alt=""></button>
+      <div class="mx-2 flex space-x-4">
+        <button
+        on:click={() => updateUser(id)}
+          ><img class="w-6" src="update-alt-2-svgrepo-com.svg" alt="" /></button
+        >
+        <button on:click={() => deleteUser(id)}
+          ><img class="w-6 h-6" src="delete-2-svgrepo-com.svg" alt="" /></button
+        >
       </div>
     </div>
     <h2>name: {name}</h2>
